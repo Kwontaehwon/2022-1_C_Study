c852168a6fe3a4a84558f9480053d99003b50703
Kwontaehwon <kygkth2011@gmail.com>
2022-06-01 05:03:25 +0900

    Week 5

---
+++ week5/value_swap.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void swap(int a, int b){
+    int tmp;
+
+    tmp = a;
+    a = b;
+    b = tmp;
+
+    printf("In Func a : %d , b : %d \n",a, b);
+}
+
+int main(){
+    int a = 22;
+    int b = 3;
+    printf("Before Swap a : %d , b : %d \n",a, b);
+    swap(a,b);
+    printf("After Swap a : %d , b : %d \n",a, b);
+
+
+}
